1fdfc2db6893644fe1c65b2c3a902134909d7a26
Richard Donohue <rgdonohue@gmail.com>
2014-01-03 12:09:20 -0600

    add divs for layout fix at smaller screen reslution width

--- include/sidebar.php
+++ include/sidebar.php
@@ -1,18 +1,25 @@
 <div id='sidebar'>
-	<h4>on the blog</h4>
-	<ul id='blog'></ul>
-	<h4>new at at the store</h4>
-	<div id="store"></div>
-	<h4>indiemapper</h4>
-	<p><a href='http://indiemapper.com' target='_blank'>Indiemapper</a> helps you make static, thematic maps from geographic data by bringing the best of traditional cartographic design to internet map-making.</p>
-	<a href='http://indiemapper.com' target='_blank'><img src='images/indiemapper.jpg' alt='indiemapper'></a>
-	<h4>colorbrewer 2.0</h4>
-	<p><a href='http://colorbrewer2.org' target='_blank'>Colorbrewer 2.0</a> is an online tool designed to help people select good color schemes for maps and other graphics.</p>
-	<a href='http://colorbrewer2.org' target='_blank'><img src='images/colorbrewer.jpg' alt='colorbrewer'></a>
-	<h4>newsletter</h4>
-	<p>Stay up-to-date with our latest news</p>
-	<form action='http://axismaps.createsend.com/t/y/s/kluir/' method='post' id='subForm'>
-		<input type='text' name='cm-kluir-kluir' id='kluir-kluir' value='Email address...'>
-		<input id='submit' type='submit' value='Subscribe'>
-	</form>
+	<div>
+		<h4>on the blog</h4>
+		<ul id='blog'></ul>
+		<h4>new at at the store</h4>
+		<div id="store"></div>
+	</div>
+	<div>
+		<h4>indiemapper</h4>
+		<p><a href='http://indiemapper.com' target='_blank'>Indiemapper</a> helps you make static, thematic maps from geographic data by bringing the best of traditional cartographic design to internet map-making.</p>
+		<a href='http://indiemapper.com' target='_blank'><img src='images/indiemapper.jpg' alt='indiemapper'></a>
+	</div>
+	<div>
+		<h4>colorbrewer 2.0</h4>
+		<p><a href='http://colorbrewer2.org' target='_blank'>Colorbrewer 2.0</a> is an online tool designed to help people select good color schemes for maps and other graphics.</p>
+		<a href='http://colorbrewer2.org' target='_blank'><img src='images/colorbrewer.jpg' alt='colorbrewer'></a>
+
+		<h4>newsletter</h4>
+		<p>Stay up-to-date with our latest news</p>
+		<form action='http://axismaps.createsend.com/t/y/s/kluir/' method='post' id='subForm'>
+			<input type='text' name='cm-kluir-kluir' id='kluir-kluir' value='Email address...'>
+			<input id='submit' type='submit' value='Subscribe'>
+		</form>
+	</div>
 </div>

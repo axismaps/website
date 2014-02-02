f1e617e4373846dd0e4ab80c7cb1f04b78a3ed4b
David Heyman <dave@axismaps.com>
2014-02-02 11:48:34 +0000

    Loads images into DB

--- php/load_projects.php
+++ php/load_projects.php
@@ -1,9 +1,12 @@
+#!/usr/bin/php
+
 <?php
 
 	include_once 'db_connect.php';
 	include_once 'Parsedown.php';
 	
 	//adding feature text
+	echo( "\nLoading feature text:\n" );
 	$files = scandir( '../data/text/highlighted/' );
 	foreach( $files as $f )
 	{
@@ -20,7 +23,36 @@
 			$query = "INSERT OR REPLACE INTO content ( id, intro, data, design, code ) VALUES( '" . $id . "', '" . SQLite3::escapeString( $matches[ 0 ][ 0 ] ) . "', '" . SQLite3::escapeString( $matches[ 0 ][ 1 ] ) . "', '" . SQLite3::escapeString( $matches[ 0 ][ 2 ] ) . "', '" . SQLite3::escapeString( $matches[ 0 ][ 3 ] ) . "' )";
 			
 			$db->query( $query );
+			
+			echo( "   $id\n" );
 		}
 	}
-
+	
+	//adding big images
+	echo( "\nLoading slideshow images:\n" );
+	$files = scandir( '../data/media/slideshow/' );
+	foreach( $files as $f )
+	{
+		if( preg_match( "/\\.png$/u", $f ) )
+		{
+			$image = file_get_contents( "../data/media/slideshow/$f" );
+			$matches = array();
+			
+			preg_match_all( "/(?<=-)[0-9]*/u", $f, $matches );
+			
+			$id = preg_replace("/-.*\\.png$/u", "", $f);
+			
+			$query = "INSERT OR REPLACE INTO images ( id, image, x, y ) VALUES( '" . $id . "', '" . $image . "', " . $matches[ 0 ][ 0 ] . ", " . $matches[ 0 ][ 1 ] . " )";
+			
+			$query = $db->prepare( "INSERT OR REPLACE INTO images ( id, image, x, y ) VALUES( ?, ?, ?, ? )" );
+			$query->bindValue( 1, $id, SQLITE3_TEXT );
+			$query->bindValue( 2, $image, SQLITE3_BLOB );
+			$query->bindValue( 3, $matches[ 0 ][ 0 ], SQLITE3_INTEGER );
+			$query->bindValue( 4, $matches[ 0 ][ 1 ], SQLITE3_INTEGER );
+			$query->execute();
+			
+			echo( "   $id\n" );
+		}
+	}
+			
 ?>

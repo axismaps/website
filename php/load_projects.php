279e791a0cd797ea03775f1f115167ebddc90be3
David Heyman <dave@axismaps.com>
2014-02-02 12:17:03 +0000

    Loads all the features images and texts in

--- php/load_projects.php
+++ php/load_projects.php
@@ -40,9 +40,7 @@
 			
 			preg_match_all( "/(?<=-)[0-9]*/u", $f, $matches );
 			
-			$id = preg_replace("/-.*\\.png$/u", "", $f);
-			
-			$query = "INSERT OR REPLACE INTO images ( id, image, x, y ) VALUES( '" . $id . "', '" . $image . "', " . $matches[ 0 ][ 0 ] . ", " . $matches[ 0 ][ 1 ] . " )";
+			$id = preg_replace( "/-.*\\.png$/u", "", $f );
 			
 			$query = $db->prepare( "INSERT OR REPLACE INTO images ( id, image, x, y ) VALUES( ?, ?, ?, ? )" );
 			$query->bindValue( 1, $id, SQLITE3_TEXT );
@@ -54,5 +52,45 @@
 			echo( "   $id\n" );
 		}
 	}
+	
+	//adding feature slideshow
+	echo( "\nLoading features slideshow:\n" );
+	$files = scandir( '../data/media/features/' );
+	foreach( $files as $f )
+	{
+		if( is_dir( "../data/media/features/$f" ) && preg_match( "/[a-z].*/u", $f ) )
+		{
 			
+			$text = file_get_contents( "../data/text/features/$f.md" );
+			$title = array();
+			$caption = array();
+			
+			preg_match_all( "/(?<=### ).*/u", $text, $title );
+			preg_match_all( "/^[^#\\n].*/um", $text, $caption );
+			
+			$db->query( "DELETE FROM features WHERE project = '$f'" );
+			
+			echo( "   $f - " );
+			
+			$sub = scandir( "../data/media/features/$f" );
+			foreach( $sub as $s )
+			{
+				if( preg_match( "/\\.png$/u", $s ) )
+				{
+					$image = file_get_contents( "../data/media/features/$f/$s" );
+					$id = intval( preg_replace( "/-.*\\.png$/u", "", $s ) );
+					$query = $db->prepare( "INSERT INTO features ( project, step, title, text, image ) VALUES( ?, ?, ?, ?, ? )" );
+					$query->bindValue( 1, $f, SQLITE3_TEXT );
+					$query->bindValue( 2, $id, SQLITE3_INTEGER );
+					$query->bindValue( 3, $title[ 0 ][ $id ], SQLITE3_TEXT );
+					$query->bindValue( 4, $caption[ 0 ][ $id ], SQLITE3_TEXT );
+					$query->bindValue( 5, $image, SQLITE3_BLOB );
+					$query->execute();
+					
+					echo( " $id" );
+				}
+			}
+			echo( "\n" );
+		}
+	}
 ?>

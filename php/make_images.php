a7829fa2728b9494d0c59e9b9845ef61835c74ac
David Heyman <dave@axismaps.com>
2014-02-03 23:19:37 +0000

    Edited Makefile to copy media directory instead of move it and added some feedback to image script.

--- php/make_images.php
+++ php/make_images.php
@@ -5,6 +5,7 @@
 	include_once 'db_connect.php';
 	include_once 'images.php';
 	
+	echo( "Loading project details...\n\n" );
 	$featured = array();
 	$results = $db->query( "SELECT id, featured FROM projects" );
 	while( $row = $results->fetchArray( SQLITE3_ASSOC ) )
@@ -14,6 +15,7 @@
 		$featured[ $id ] = $row;
 	}
 	
+	echo( "Building media directories...\n\n" );
 	if( file_exists( dirname(__FILE__) . '/../media' ) )
 	{
 		rm_folder( dirname(__FILE__) . '/../media' );
@@ -21,6 +23,7 @@
 	mkdir( dirname(__FILE__) . '/../media', 0777, true );
 	mkdir( dirname(__FILE__) . '/../media/slideshow', 0777, true );
 	
+	echo( "Creating slideshow images:\n" );
 	$results = $db->query( "SELECT * FROM images" );
 	while( $row = $results->fetchArray( SQLITE3_ASSOC ) )
 	{
@@ -33,27 +36,35 @@
 			$featured[ $id ][ 'featured' ] = $f;
 		}
 		file_put_contents( dirname(__FILE__) . "/../media/slideshow/" . $id . ".png" , $row[ 'image' ] );
+		echo( "   $id.png\n" );
 	}
+	echo( "\n" );
 	
 	mkdir( dirname(__FILE__) . '/../media/icon', 0777, true );
 	mkdir( dirname(__FILE__) . '/../media/portfolio', 0777, true );
 	
+	echo( "Creating portfolio images:\n" );
 	foreach( $featured as $id => $el )
 	{
 		if( isset( $el[ 'image' ] ) )
 		{
 			if( $el[ 'featured' ] == 1 )
 			{
+				echo( "   icon/$id.png\n" );
 				imagepng( resample( $el[ 'image' ], 70, 70, $el[ 'x' ], $el[ 'y' ], false ), dirname(__FILE__) . "/../media/icon/" . $id . ".png" );
+				echo( "   portfolio/$id.png\n" );
 				imagepng( resample( $el[ 'image' ], 265, 185, $el[ 'x' ], $el[ 'y' ], false ), dirname(__FILE__) . "/../media/portfolio/" . $id . ".png" );
 			}
 			else
 			{
+				echo( "   portfolio/$id.png\n" );
 				imagepng( resample( $el[ 'image' ], 815, 255, $el[ 'x' ], $el[ 'y' ], false ), dirname(__FILE__) . "/../media/portfolio/" . $id . ".png" );
 			}
 		}
 	}
+	echo( "\n" );
 	
+	echo( "Creating feature images:\n" );
 	mkdir( dirname(__FILE__) . '/../media/features', 0777, true );
 	$results = $db->query( "SELECT project, step, image FROM features" );
 	while( $row = $results->fetchArray( SQLITE3_ASSOC ) )
@@ -62,6 +73,7 @@
 		{
 			mkdir( dirname(__FILE__) . '/../media/features/' . $row[ 'project' ], 0777, true );
 		}
+		echo( '   features/' . $row[ 'project' ] . "/" . $row[ 'step' ] . ".png\n" );
 		imagepng( resample( $row[ 'image' ], 640, 520, null, null, false ), dirname(__FILE__) . '/../media/features/' . $row[ 'project' ] . "/" . $row[ 'step' ] . ".png" );
 	}
 	

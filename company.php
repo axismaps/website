ee40c47f567813beb86eb248ffe00a2431aa55cc
David Heyman <dave@axismaps.com>
2014-02-24 15:13:45 +0000

    First shot at a finalized company page

--- company.php
+++ company.php
@@ -11,19 +11,45 @@
 		<div id='container'>
 			<?php include('include/header.php'); ?>
 			<?php include('include/nav.php'); ?>
+			<section id="trio">
+				<h2 class="ribbon">We are Axis Maps:</h2>
+				<article id="dave" style="height:auto">
+					<div class="headshot"></div>
+					<h3>Dave</h3>
+					<ul>
+						<li>Managing Director</li>
+						<li>Development backup</li>
+						<li>Chief organizer</li>
+					</ul>
+				</article>
+				<article id="ben" style="height:auto">
+					<div class="headshot"></div>
+					<h3>Ben</h3>
+					<ul>
+						<li>UI and map designer</li>
+						<li>Map store coordinator</li>
+						<li>Stickler for details</li>
+					</ul>
+				</article>
+				<article id="andy" style="height:auto">
+					<div class="headshot"></div>
+					<h3>Andy</h3>
+					<ul>
+						<li>Lead developer</li>
+						<li>Cartographic stalwart</li>
+						<li>Academic cartography ambassador</li>
+				</article>
+			</section>
+			<img src="images/map.jpg" style="width:100%" />
 			<section class='axis-summary' id="history">
-				<h2 class='ribbon'>Company Past</h2>
-				<p>Axis Maps was formed in 2006 by 3 graduate students finishing their advanced degrees in Geography at the University of Wisconsin-Madison. Having originally entered grad school to study traditional print cartography and GIS, we quickly became engaged with the rapidly changing field of interactive mapping. Google Maps had launched the previous spring, while more and more internet users were growing accustomed to having interactive maps as a part of their online lives. However, we were surprised to see that the cartographic fundamentals and traditions we had been studying were largely absent from these new maps. Instead, these maps focused on the technical aspects of delivering geographic content over the web at the expense of clear communication through cartographic design.</p>
-				<p>We formed Axis Maps to bring Cartography to a field that was being dominated by Computer Science. While other companies focused on algorithms, we developed intuitive user-interfaces. Where they were building software to reach the broadest market possible, we designed custom maps to make sure each map was right for our customer.</p>
+				<h2 class='ribbon'>Our Story</h2>
+				<p>Axis Maps was formed in 2006 by 3 graduate students finishing their advanced degrees in Geography at the University of Wisconsin-Madison. Having originally entered grad school to study traditional print cartography and GIS, we quickly became engaged with the rapidly changing field of interactive mapping. Google Maps had launched the previous spring, while more and more internet users were growing accustomed to having interactive maps as a part of their online lives. However, we were surprised to see that the cartographic fundamentals and traditions we had been studying were falling by the wayside. Instead, these new maps focused on the technical aspects of delivering geographic content over the web rather than clear communication through cartographic design.</p>
+				<p>We formed Axis Maps to bring Cartography to what was becoming a technical field. While other companies focused on algorithms, we developed intuitive user-interfaces. Where they were building software to reach the broadest market possible, we designed custom maps to make sure each map was right for our client. We believed that new map technologies could be used not only for efficiency and delivery, but also for great cartographic and interactive design.</p>
 				<p>Since then, we’ve been pleased to work with a diverse range of companies including : Navteq, Embassy of Canada, Commonwealth Fund, Bank of America, Etihad Airways, Emirates Airlines, Hearst Publishing, IPRO, and The New York Times.</p>
 				<p>Our work has been written about and featured in: The Boston Globe, The Atlantic, Gizmodo, Fast Company, and Directions Magazine.</p>
-				<p>After 7 years, our company has grown to include more than just custom interactive maps. In the Spring of 2010, we launched indiemapper (http://indiemapper.com), our first commercial product to help people make better maps. We designed and built indiemapper because we were frustrated with the existing mapping tools and wanted to bring the best practices of cartographic design to the 21st century mapping workflow. More recently, we’ve returned to our roots in manual cartography producing a series of city-posters that accurately depict the streets of a city using nothing but type (http://store.axismaps.com). By weaving together thousands of words, a full picture of the city emerges. Every letter was carefully placed, taking hundreds of hours to complete for each map.</p>
+				<p>After 8 years, our company has grown to include more than just custom interactive maps. In the Spring of 2010, we launched indiemapper (http://indiemapper.com), our first commercial product to help people make better maps. We designed and built indiemapper because we were frustrated with the existing mapping tools and wanted to bring the best practices of cartographic design to the 21st century mapping workflow. More recently, we’ve returned to our roots in manual cartography producing a series of city-posters that accurately depict the streets of a city using nothing but type (http://store.axismaps.com). By weaving together thousands of words, a full picture of the city emerges. Every letter was carefully placed, taking hundreds of hours to complete for each map.</p>
 			</section>
 			<div style="clear:both"></div>
-			<section id="bios">
-				<h2 class='ribbon'>Company Present</h2>
-				<img src="images/map.jpg" />
-			</section>
 			<?php include('include/footer.php'); ?>
 		</div><!-- end container -->
 		<script src="js/jquery.js"></script>

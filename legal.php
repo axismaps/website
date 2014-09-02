e9df713a6a7c172fd8252dcfc17b1fa4eed0f4a5
David Heyman <dave@axismaps.com>
2014-09-02 10:16:20 +0100

    Added legal text

---
+++ legal.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Axis Maps Company - Cartography. Visualization. Design.</title>
+		<meta name="description" content="Axis Maps Data Design Code">
+		<?php include('include/meta.php'); ?>
+	</head>
+	<body>
+		<div id='wrapper'>
+			<?php include('include/header.php'); ?>
+			<?php include('include/nav.php'); ?>
+			<?php include('legal/' . $_GET[ 't' ] . '.php' ); ?>
+		</div>
+	</body>
+</html>
